00acd855c79bd3feb7c42ee1c72c72a42717246f
erick-a11y <erick123g1234@gmail.com>
2025-01-07 21:21:14 -0300

    2

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print("hola mundo")
+nombre="eric marin"
+edad=20
+
+print("mi nombre es",nombre,"y mi edad es",edad)
+
+
+
+num1=0
+num2=0
+
+print("ingresa un numero")
+num1=int(input())
+print("ingresa otro numero")
+num2=int(input())
+
+print(num1+num2)
+
+
+print("ingresa un numero")
+num1=int(input())
+if num1>0:
+    print("el numero es positivo")
+else:
+    print("el numero es negativo o cero")
+    
+    
